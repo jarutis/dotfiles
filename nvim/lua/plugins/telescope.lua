1364d62321592704ce4d536e6597768858a88674
Jonas Jarutis <jarutis@gmail.com>
2025-05-11 18:01:42 +0300

    Add Telescope CmdLine

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,43 +1,48 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"jonarrien/telescope-cmdline.nvim",
+		},
+		keys = {
+			{ "<leader><leader>", "<cmd>Telescope cmdline<cr>", desc = "Cmdline" },
+		},
 
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "node_modules", ".git", ".next" },
-      },
-    },
+		opts = {
+			defaults = {
+				file_ignore_patterns = { "node_modules", ".git", ".next" },
+			},
+		},
 
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>hh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end,
-  },
-  {
-    "piersolenski/telescope-import.nvim",
-    dependencies = "nvim-telescope/telescope.nvim",
-    config = function()
-      require("telescope").load_extension("import")
-      vim.keymap.set("n", "<leader>i", ":Telescope import<cr>", {})
-    end,
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>hh", builtin.help_tags, { desc = "Telescope help tags" })
+		end,
+	},
+	{
+		"piersolenski/telescope-import.nvim",
+		dependencies = "nvim-telescope/telescope.nvim",
+		config = function()
+			require("telescope").load_extension("import")
+			vim.keymap.set("n", "<leader>i", ":Telescope import<cr>", {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
-
